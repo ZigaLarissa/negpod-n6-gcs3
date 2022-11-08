484a54248d4369f2dc45c3cf71bfe7ff70acb55d
B.Larissa <109439888+ZigaLarissa@users.noreply.github.com>
2022-11-08 20:06:31 +0200

    conditional actions on integer

---
+++ conditional_01.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+m = int(input("write any integer: "))
+if m % 2 == 0:
+    print("Weird")
+else:
+    if m >= 2 and m <= 5:
+        print("Not Weird")
+    elif m >= 6 and m <= 20:
+        print("Weird")
+    elif m > 20:
+        print("Not Weird")
